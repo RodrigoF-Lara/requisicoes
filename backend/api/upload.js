9a1c2717af2bf73aa1195380b33cd3b970c7883f
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-08-08 15:31:18 -0300

    Update upload.js

--- backend/api/upload.js
+++ backend/api/upload.js
@@ -11,6 +11,10 @@ const config = {
 };
 
 export default async function handler(req, res) {
+    if (req.method === "GET") {
+        return res.status(200).json({ message: "API online e pronta para receber uploads!" });
+    }
+
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Método não permitido" });
     }
